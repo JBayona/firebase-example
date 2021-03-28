90ac8cbeca91f67d898a91a7986a830065440a19
JBayona <jordavids_22@hotmail.com>
2021-03-28 12:42:19 -0600

    Add firebase config and use env variables.

---
+++ functions/src/config/firebase.ts
@@ -0,0 +1,15 @@
+import * as admin from "firebase-admin";
+import * as functions from "firebase-functions";
+
+// Importing the Firebase admin for initializing our app and Firebase functions for accessing the Firebase environment variables
+admin.initializeApp({
+  credential: admin.credential.cert({
+    privateKey: functions.config().private.key.replace(/\\n/g, "\n"),
+    projectId: functions.config().project.id,
+    clientEmail: functions.config().client.email,
+  }),
+  databaseURL: "https://journal-rest-api-22035.firebaseio.com",
+});
+
+const db = admin.firestore();
+export { admin, db };
