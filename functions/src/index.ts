1c24d0ea06e3bbd83e11853a69d0568f5c5fba5e
JBayona <jordavids_22@hotmail.com>
2021-03-28 12:25:01 -0600

    Integrate Express.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,17 @@
 import * as functions from "firebase-functions";
+import * as express from "express";
+
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+const app = express();
+app.get("/", (req, res) => res.status(200).send("Hey there!"));
+
+// Server rotutes
+exports.app = functions.https.onRequest(app);
